ef27b51de404c9eaa55cbabde011346c6cda6b2d
Love, Kumar <love91k@gmail.com>
2021-06-07 18:28:54 +0000

    unix_script

--- process_script/script.sh
+++ process_script/script.sh
@@ -5,12 +5,13 @@ then
 else
 	process_num=1
 fi
-expr=`expr $process_num + 1` #first process is header need to increase 1
+process_num=`expr $process_num + 1` #first process is header need to increase 1
 header="_______Process Name________%CPU %MEM PID PPID PORT"
 record=$( ps -eo cmd:50,%cpu,%mem,pid,ppid --sort=-%cpu,-%mem|   ps -eo cmd,%cpu,%mem,pid,ppid --sort=-%cpu,-%mem | head -$process_num | tail -1)
 pid=$(echo $record | awk '{print $5}')
+port=$(netstat -ano -p tcp | grep LISTEN | grep $pid | awk '{print $4}' | awk -F":" '{print $2}')
+record1="$record $port"
 
 
 echo $header 
-echo $record
-echo pid is $pid
+echo $record1
